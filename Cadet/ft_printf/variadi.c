d05efb573459c50db84663702cbed6666c47cf1e
MaxKuftinec <116775534+MaxKuftinec@users.noreply.github.com>
2024-06-22 10:57:53 +0800

    Update variadi.c

--- Cadet/ft_printf/variadi.c
+++ Cadet/ft_printf/variadi.c
@@ -16,3 +16,29 @@ int main()
     printf("%d\n", sum(3, 42, 13, 123 ,123 ,123));
     return 0;
 }
+
+/*
+int foo(char *p, ...)
+{
+    va_list x;
+    int i = 0;
+    int num_of_flag = 0;
+    int sum = 0;
+    
+    while(p[i])
+        if (p[i++] = '%')
+            num_of_flag++;
+    printf("%d\n", num_of_flag);
+    va_start(x, p);
+    for(int i = 0; i < num_of_flag; i++)
+        sum += va_arg(x, int);
+    return (va_end(x), sum);
+}
+
+int main()
+{
+    printf("%d\n", foo("%d%d%d%d", 1, 2, 3, 4));
+
+    return 0;
+}
+*/
