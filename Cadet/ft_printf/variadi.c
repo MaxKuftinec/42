aca3353a0c5c25eead7ab725b1a30d2925bfa1ae
MaxKuftinec <116775534+MaxKuftinec@users.noreply.github.com>
2024-06-28 00:06:31 +0800

    Update variadi.c

--- Cadet/ft_printf/variadi.c
+++ Cadet/ft_printf/variadi.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int sum(int n, ...)
-{
-    va_list x;
-    int sum = 0;
-    va_start(x, n);
-    for(int i = 0; i < n; i++)
-        sum += va_arg(x, int);
-    return (va_end(x), sum);
-}
-
-int main()
-{
-    printf("%d\n", sum(3, 42, 13, 123 ,123 ,123));
-    return 0;
-}
-
-/*
 int foo(char *p, ...)
 {
     va_list x;
-    int i = 0;
-    int num_of_flag = 0;
-    int sum = 0;
+    char flagnum = 0;
     
-    while(p[i])
-        if (p[i++] = '%')
-            num_of_flag++;
-    printf("%d\n", num_of_flag);
     va_start(x, p);
-    for(int i = 0; i < num_of_flag; i++)
-        sum += va_arg(x, int);
-    return (va_end(x), sum);
+    p--;
+    while (*++p)
+    { 
+        if ('%' == *p)
+        {
+            if (flagnum++, 100 == p[sizeof(char)])
+                ft_putnbr((p++, va_arg(x, int)));
+            else if (99 == p[sizeof(char)])
+                write((p++, 1), &(char){va_arg(x, int)}, sizeof(char));
+            else if ('%' == p[sizeof(char)])
+                write((p++, 1), &(char){'%'}, sizeof(char));
+            else if ('p' == p[sizeof(char)])
+                dectohex((p++, va_arg(x, size_t)));
+        }
+        else
+            write(1, p, 1);
+    }
+    return (va_end(x), flagnum);
 }
 
 int main()
@@ -41,4 +33,3 @@ int main()
 
     return 0;
 }
-*/
